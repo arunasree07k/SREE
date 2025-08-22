d3775590ef60fcf9e4265c0e32c7671cbdc91124
aruna <arunasree07k@gmail.com>
2025-08-22 11:35:40 +0530

    first commit

---
+++ src/gitexample/Sample.java
@@ -0,0 +1,10 @@
+package gitexample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("JAVA PROGRAMMING");
+		System.out.println("DIGI BRAINS ACADEMY");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Aruna Sree
+ *
+ */
+module SREE {
+}
